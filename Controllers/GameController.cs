0a363ba8c53d192d465bfc43c83d3a704f82138c
SaffronG <133020249+SaffronG@users.noreply.github.com>
2025-04-23 12:12:08 -0600

    updates to board generation and matchmaking

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -1,7 +1,9 @@
 namespace StrategoGameServer.Controllers
 {
+    using System.Diagnostics;
     using System.Net.Mime;
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.ObjectPool;
     using StrategoGameServer.Records;
 
     [ApiController]
@@ -10,25 +12,54 @@ namespace StrategoGameServer.Controllers
     [ApiConventionType(typeof(DefaultApiConventions))]
     public class GameController : ControllerBase
     {
+        internal static Stack<int> DefaultList() {
+            return new(
+            [
+                -2, // FLAG 
+                -1, -1, -1, -1, -1, -1, // BOMBS
+                0, // SPY
+                9, 9, 9, 9, 9, 9, 9, 9, // SCOUTS 
+                8, 8, 8, 8, 8, // MINERS
+                7, 7, 7, 7, // SERGEANTS
+                6, 6, 6, 6, // LIEUTENANTS
+                5, 5, 5, 5, // CAPTAINS
+                4, 4, 4, // MAJORS
+                3, 3, // COLONELS
+                2, // GENERAL
+                1 // MARSHAL
+            ]);
+        }
+
+        internal static int[] RandList(Stack<int> defaultList) {
+            int[] tmp = new int[40];
+            for (int i = 0; i < 40; i++) tmp[i] = -3;
+            while (tmp.Length < defaultList.Count) {
+                int rand = Random.Shared.Next(0, tmp.Length);
+                if (tmp[rand] == -3) tmp[rand] = defaultList.Pop();
+            }
+            return tmp;
+        }
         internal static List<Game> Games =
         [
             new ("ProGamer69", "Strat3g1st", InitBoard(), []),
-            new ("test", null, InitBoard(), []),
         ];
-
         internal static Piece[] InitBoard()
         {
+            int User_a_count = 0;
+            int User_b_count = 0;
+            var User_a = RandList(DefaultList());
+            var User_b = RandList(DefaultList());
             var board = new Piece[100];
             Random r = new();
             for (int i = 0; i < 100; i++)
             {
                 if (i < 40)
                 {
-                    board[i] = new(r.Next(-2, 12), "red");
+                    board[i] = new(User_a[User_a_count++], "user_a");
                 }
                 else if (i > 59)
                 {
-                    board[i] = new(r.Next(-2, 12), "blue");
+                    board[i] = new(User_b[User_b_count++], "user_b");
                 }
                 else
                 {
@@ -41,10 +72,6 @@ namespace StrategoGameServer.Controllers
         [HttpGet("getGames")]
         public IActionResult GetGames()
         {
-            if (Games.Count < 1)
-            {
-                return Ok("No Games Found");
-            }
             return Ok(Games);
         }
 
@@ -56,21 +83,22 @@ namespace StrategoGameServer.Controllers
             for (int i = 0; i < Games.Count; i++)
             {
                 // FIND OPEN GAME
-                if (Games[i].User_b is null)
+                if (Games[i].User_b is null || Games[i].User_a is null)
                 {
                     openGame = Games[i];
                     LobbyID = i;
                     break;
                 }
-                if (Games[i].User_a == user.Username || Games[i].User_b == user.Username) {
+                 if (Games[i].User_a == user.Username || Games[i].User_b == user.Username) {
                     return Ok(new GameContext(i.ToString(), Games[i].Board, user.Username, Games[i].Moves!.Count, false));
                 }
             }
             if (openGame == null)
             {
                 // CREATE A NEW GAME
-                Games[LobbyID] = new(user.Username, null, new Piece[100], []);
+                Games[LobbyID] = new(user.Username, null, InitBoard(), []);
                 Games.Add(openGame!);
+                LobbyID = Games.Count - 1;
                 return Ok(new GameContext(LobbyID.ToString(), Games[LobbyID].Board, user.Username, 0, false));
 
             }
--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # StrategoGameServer
 A simple game server for a multiplayer Stratego application. This server is in charge of account persistence, stats, and matchmaking.
+
+# StrategoGameServer API Documentation
+
+Explore the available endpoints for the StrategoGameServer API.
+
+## Endpoints
+
+### HomeController
+- **GET /**  
+  Serves the `map.html` file as the homepage.
+
+### AuthController
+- **POST /api/auth/login**  
+  Logs in a user with their credentials.
+
+- **POST /api/auth/register**  
+  Registers a new user account.
+
+- **POST /api/auth/logout**  
+  Logs out a currently authenticated user.
+
+- **GET /api/auth/concurrentUsers**  
+  Retrieves a list of currently authenticated users.
+
+- **GET /api/auth/getAccount?username=**  
+  Retrieves account details for a specific username.
+
+- **GET /api/auth/logoutAll**  
+  Logs out all currently authenticated users.
+
+### GameController
+- **GET /api/game/getGames**  
+  Retrieves a list of all active games.
+
+- **POST /api/game/findGame**  
+  Attempts to find a game for the user.
+
+- **POST /api/game/postMove**  
+  Submits a move for the current game.
+
+- **DELETE /api/game/endGame**  
+  Ends the current game session.
+
+- **POST /api/game/getBoard**  
+  Retrieves the game board for a specific lobby and color.
+
+- **GET /api/game/ResetBoard**  
+  Resets the game board to its initial state.
